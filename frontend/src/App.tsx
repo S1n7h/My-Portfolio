538f7b08c3ebfaf645ce377760cec0c87e15c197
Soumyadeep Mullick <soumyadeepmullick7@gmail.com>
2026-09-11 20:28:52 +0530

    REEEE

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-950 p-6">
       <header className="text-center my-4">
-        <h1 className="text-3xl font-bold text-white">My AI Portfolio</h1>
+        <h1 className="text-3xl font-bold text-white">An Introvert's Dreamspace</h1>
       </header>
       
       <main>
